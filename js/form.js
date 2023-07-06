64bff90348bd9247867ac623ea7a1298a1930b21
jalilmboyzt <kirimkejalil@gmail.com>
2023-07-06 13:00:50 +0900

    menambah validasi form pada field nama, commit

--- js/form.js
+++ js/form.js
@@ -9,24 +9,29 @@ document.getElementById('contact-form').addEventListener('submit', function(even
   var userId = document.getElementById('userId').value;
 
   // Reset pesan kesalahan sebelumnya
+  document.getElementById('name-error').innerHTML = '';
   document.getElementById('email-error').innerHTML = '';
   document.getElementById('message-error').innerHTML = '';
 
   // Validasi input
   var isValid = true;
+  
+  if (!validateName(name)) {
+  document.getElementById('name-error').innerHTML = 'Nama hanya boleh berisi huruf, angka, dan spasi';
+  isValid = false;
+}
 
   if (!validateEmail(email)) {
     document.getElementById('email-error').innerHTML = 'Email tidak valid';
     isValid = false;
   }
 
-  if (message === '') {
-    document.getElementById('message-error').innerHTML = 'Pesan harus diisi';
+  if (message.length < 15) {
+    document.getElementById('message-error').innerHTML = 'Pesan harus memiliki minimal 15 karakter';
     isValid = false;
   }
 
-  // Jika input valid, lakukan pengiriman form
-  if (isValid) {
+if (isValid) {
     // Buat objek data yang akan dikirim ke server
     var data = {
       name: name,
@@ -47,14 +52,18 @@ localStorage.setItem('formData', JSON.stringify(data));
     showCancelButton: true,
     confirmButtonText: 'Lihat Data',
     cancelButtonText: 'Tutup',
+    timer: 3000,
     allowOutsideClick: false
   }).then(function(result) {
+    setTimeout(function() {
+      // Muat ulang halaman setelah 3 detik
+      window.location.reload();
+    }, 500);
     if (result.isConfirmed) {
       // Redirect ke domain.com/data.html
       window.location.href = './data.html';
     }
   });
-
   }
 });
 
@@ -62,4 +71,9 @@ localStorage.setItem('formData', JSON.stringify(data));
 function validateEmail(email) {
   var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return re.test(email);
+}
+
+function validateName(name) {
+  var regex = /^[a-zA-Z0-9\s]+$/;
+  return regex.test(name);
 }
